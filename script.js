f22777b90d4c328035c728b3117ae2148cf61700
thezier <100000131+thezier@users.noreply.github.com>
2022-09-23 07:25:17 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.projects_tab');
+const tabsContainer = document.querySelector('.projects_tab-container');
+const tabsContent = document.querySelectorAll('.projects_content');
+const section1 = document.querySelector('#section--1');
+const allSections = document.querySelectorAll('.section');
+
+/// Nav
+document.querySelector('.nav_links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav_link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav_link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav_link');
+    const logo = link.closest('.nav').querySelector('.nav_logo');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.6));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+/////////////////////////////////////
+// Sticky Navigation
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// Reveal sections
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '20px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
