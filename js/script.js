6e4544c60034e40cd8f15fa7175d7b31d6bc9212
thezier <100000131+thezier@users.noreply.github.com>
2023-03-28 12:36:58 -0700

    Fit to blur fliter bug on Safari
    Blur would get stuck and cause performance drops on Safari. Adding "transform" in CSS along with blur has seemed to fix this problem.

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ const loadImg = function (entries, observer) {
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
-  threshold: 0,
+  threshold: 0.5,
   rootMargin: '20px',
 });
 
